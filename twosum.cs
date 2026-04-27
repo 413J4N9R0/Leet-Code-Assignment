7df23dbf76e7ebdd02b2359dcc1d90a2bc3e6605
413J4N9R0 <aramirez8840@interbayamon.edu>
2026-04-27 10:10:39 -0400

    Update twosum.cs

--- twosum.cs
+++ twosum.cs
@@ -1,20 +1,19 @@
 public class Solution {
-public int[] TwoSum(int[] nums, int targetnum) {
-Dictionary<int, int> Nums = new Dictionary<int, int>();
+public int[] TwoSum(int[] nums, int targetnum) { //crea arreglo de numeros y assigna un target number
+Dictionary<int, int> Nums = new Dictionary<int, int>(); //crea hashmap para GET 0(1); hashmaps (dictionaries) utilizan indice con valor
 
-for (int i = 0; i < nums.Length; i++)
+for (int i = 0; i < nums.Length; i++) //va por los numeros en el diccionario
 {
 int currentNum = nums[i];
-int NumNeeded = targetnum - currentNum;
+int NumNeeded = targetnum - currentNum; //calcula cuantos hops hasta el numero requerido
 
 if (Nums.ContainsKey(NumNeeded))
 {
-return new int[] { Nums[NumNeeded], i };
+return new int[] { Nums[NumNeeded], i }; //si lo encuentra, regresa el numero y su indice
 }
 
 Nums[currentNum] = i;
 }
-
-return new int[] { };
+return new int[] { }; //si no encuentra nada, devuelve un arreglo vacio
 }
 }
