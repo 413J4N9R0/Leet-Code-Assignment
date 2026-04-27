7d586ef7e57ab1f7628e97c3ebea06287bf9071e
413J4N9R0 <aramirez8840@interbayamon.edu>
2026-04-27 10:06:18 -0400

    Add comments to clarify palindrome logic

---
+++ palindrome.cs
@@ -0,0 +1,18 @@
+
+public class Solution {
+    public bool IsPalindrome(int x) {
+         if (x < 0){
+            return false; //Any negative number is ruled out
+        } 
+
+     int og = x;
+     int numReversed = 0;
+
+     while (x > 0){
+
+     numReversed = numReversed * 10 + x % 10; // shifts numbers to the left; grabs last digit.
+     x /= 10; //removes last digit
+     }
+      return og == numReversed; //valida
+    }
+    }
