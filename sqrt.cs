fc7192b7363f3071dd97c5a1325ba558b44184b6
413J4N9R0 <aramirez8840@interbayamon.edu>
2026-04-27 10:15:47 -0400

    Create sqrt.cs

---
+++ sqrt.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int MySqrt(int x) {
+          if (x < 2)
+        return x; //cualquier valor menos 2 no tiene root
+
+    int i = 1;
+
+    //Avoids overflow by dividing by i, maintaining the number below the designated length of int
+    while (i <= x / i) 
+    {
+        i++;
+    }
+
+    
+    return i - 1; //el loop trabaja hasta que i sea mayor, cual si se pasa, hay que volver al paso anterior para obtener resultado.
+
+    }
+}
